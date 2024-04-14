820a8a55dbc6cfca2ce3756c3180d6a55e673b4d
BrianFellDownTheWell <123607721+BrianFellDownTheWell@users.noreply.github.com>
2024-04-14 14:53:02 -0700

    Combined the sound trigger script with the script for the shells.

--- Assets/Scripts/Shell.cs
+++ Assets/Scripts/Shell.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Shell : MonoBehaviour
 {
+    public AudioClip testClip;
+    public AudioSource testAudio;
+
     private static int triggerCount = 0;
     private bool isTriggered;
     public Chest chest;
@@ -11,11 +14,13 @@ public class Shell : MonoBehaviour
     void start()
     {
         isTriggered = false;
+        testAudio = gameObject.GetComponent<AudioSource>();
     }
     private void OnTriggerEnter2D(Collider2D c)
     {
         if (c.CompareTag("Player") && !isTriggered)
         {
+            testAudio.PlayOneShot(testClip, 1);
             triggerCount++;
             isTriggered = true;
             Debug.Log(triggerCount);
